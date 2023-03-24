e3b4d02758d9acad831c9dd31616de5365800773
Alice <alicezheng888@gmail.com>
2023-03-24 13:12:20 +0100

    fikset mini greir

--- src/main/java/minesweeperproject/MinesweeperApp.java
+++ src/main/java/minesweeperproject/MinesweeperApp.java
@@ -5,19 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class MinesweeperApp extends Application {
 
-    public static void main(String[] args) {
-        Application.launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Minesweeper");
+        primaryStage.setScene(new Scene(FXMLLoader.load(MinesweeperApp.class.getResource("Minesweeper.fxml"))));
+        primaryStage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        primaryStage.setTitle("Minesweeper");
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("App.fxml"))));
-        primaryStage.show();
+    public static void main(String[] args) {
+        MinesweeperApp.launch(args);
     }
 
 }
