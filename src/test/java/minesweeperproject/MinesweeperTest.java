7b829e1f6283146b9087b583872fe0f9a3ea745c
Alice <alicezheng888@gmail.com>
2023-03-24 20:33:30 +0100

    prøver

--- src/test/java/minesweeperproject/MinesweeperTest.java
+++ src/test/java/minesweeperproject/MinesweeperTest.java
@@ -1,5 +1,11 @@
 package minesweeperproject;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class MinesweeperTest {
-    
+    @Test
+    public void testConstructor() {
+
+    }
 }
