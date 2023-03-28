576991b81dcf64aad4728a78bafa49a5c7197270
philip <philipvl@stud.ntnu.no>
2023-03-28 23:36:17 +0200

    fxml

--- src/main/java/minesweeperproject/MinesweeperController.java
+++ src/main/java/minesweeperproject/MinesweeperController.java
@@ -1,5 +1,9 @@
 package minesweeperproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MinesweeperController {
-    
+    @FXML
+    private Button easyButton, mediumButton, hardButton, customButton;
 }
